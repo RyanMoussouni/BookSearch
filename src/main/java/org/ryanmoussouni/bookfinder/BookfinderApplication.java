4dd3c3d49e15582d369357d34143d3781f16227a
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-08 14:44:02 +0200

    Add BookFinderApplication.java

---
+++ src/main/java/org/ryanmoussouni/bookfinder/BookfinderApplication.java
@@ -0,0 +1,13 @@
+package org.ryanmoussouni.bookfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookfinderApplication.class, args);
+	}
+
+}

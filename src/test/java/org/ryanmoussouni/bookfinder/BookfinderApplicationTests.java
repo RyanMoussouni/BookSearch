b390ad2b76caf4a05c3adc3318d35e0631fe58d4
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-08 14:43:11 +0200

    First commit

---
+++ src/test/java/org/ryanmoussouni/bookfinder/BookfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ryanmoussouni.bookfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

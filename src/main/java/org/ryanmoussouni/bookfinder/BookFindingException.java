7275492f8f111629096b0e3ea4b23baf7c0c7a59
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-22 19:16:56 +0200

    Add BookFindingException

--- src/main/java/org/ryanmoussouni/bookfinder/BookFindingException.java
+++ src/main/java/org/ryanmoussouni/bookfinder/BookFindingException.java
@@ -1,2 +1,7 @@
-package org.ryanmoussouni.bookfinder;public class BookFindingException {
+package org.ryanmoussouni.bookfinder;
+
+public class BookFindingException extends Throwable {
+    public BookFindingException(String message, BookFetchingException e) {
+        super(message, e);
+    }
 }

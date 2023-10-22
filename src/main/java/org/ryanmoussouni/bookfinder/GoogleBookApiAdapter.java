91b8bb2fc082e3d065106c01e318cbb37e82f158
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-22 19:17:36 +0200

    Add GoogleBookApiAdapter

--- src/main/java/org/ryanmoussouni/bookfinder/GoogleBookApiAdapter.java
+++ src/main/java/org/ryanmoussouni/bookfinder/GoogleBookApiAdapter.java
@@ -1,2 +1,34 @@
-package org.ryanmoussouni.bookfinder;public class BookFinderImpl {
+package org.ryanmoussouni.bookfinder;
+
+import org.ryanmoussouni.bookfinder.domain.Book;
+import org.ryanmoussouni.bookfinder.domain.googlebooks.Volume;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GoogleBookApiAdapter implements BookFinder {
+    private VolumesFetcher googleApiClient;
+
+    @Override
+    public List<Book> findBookByTitle(String title) throws BookFindingException {
+        try {
+            var volumes = googleApiClient.performListVolumesRequest();
+            return volumes.getItems()
+                    .stream()
+                    .map(this::mapVolumeToBook)
+                    .collect(Collectors.toList());
+        } catch (BookFetchingException e) {
+            e.printStackTrace();
+            var message = "A book could not be found";
+            throw new BookFindingException(message, e);
+        }
+    }
+
+    private Book mapVolumeToBook(Volume volume) {
+        return Book.builder()
+                .title(volume.getVolumeInfo().getTitle())
+                .description(volume.getVolumeInfo().getDescription())
+                .authors(volume.getVolumeInfo().getAuthors())
+                .build();
+    }
 }

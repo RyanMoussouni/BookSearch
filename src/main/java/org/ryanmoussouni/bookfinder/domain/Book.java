709bcfa08277eb1c6234eaac371ea72b32501887
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-22 19:17:28 +0200

    Update Book

--- src/main/java/org/ryanmoussouni/bookfinder/domain/Book.java
+++ src/main/java/org/ryanmoussouni/bookfinder/domain/Book.java
@@ -1,4 +1,15 @@
 package org.ryanmoussouni.bookfinder.domain;
 
+import lombok.Builder;
+
+import java.util.Date;
+import java.util.List;
+
+@Builder
 public class Book {
+    private String title;
+    private List<String> authors;
+    private Date publicationDate;
+    private String ISBN;
+    private String description;
 }
